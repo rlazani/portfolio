c7dde3bcb27eb057a8d230dee2114de2f4fcb5cd
rlazani <rlazani@hotmail.com>
2024-03-04 16:19:16 -0300

    mudança experiencia/projetos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { initMenu } from "./src/menu/Controller.js";
 import { ControllerProjetos } from "./src/projetos/Controller.js";
 import { ControllerConhecimentos } from "./src/conhecimentos/Controller.js";
 
-initHeader();
+//initHeader();
 initMenu();
 
 const constrollerProjetos = new ControllerProjetos();
