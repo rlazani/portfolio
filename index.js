e2a095a7eaa7f1d9f96c357961d7485875232664
rlazani <rlazani@hotmail.com>
2024-03-06 15:58:04 -0300

    adicionando classe experiencia

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //import { initHeader } from "./src/header/Controller.js";
 import { initMenu } from "./src/menu/Controller.js";
-import { ControllerProjetos } from "./src/projetos_recentes/Controller.js";
-//import { ControllerConhecimentos } from "./src/conhecimentos/Controller.js";
+import { ControllerProjetos } from "./src/projetos/Controller.js";
+import { ControllerExperiencia } from "./src/experiencia/Controller.js";
 
 //initHeader();
-//initMenu();
+initMenu();
 
 //const constrollerProjetos = new ControllerProjetos();
 //const constrollerConhecimentos = new ControllerConhecimentos();
 
-const IniciandoProjetos = new ControllerProjetos();
+const iniciandoProjetos = new ControllerProjetos();
+const iniciandoExperiencia = new ControllerExperiencia();
